c2bcfef81595f5604d1afa4935ed53bf686015fa
amiller0806 <ariel.miller555@gmail.com>
2022-12-18 13:39:32 -0500

    fixed test files and added to git ignore

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,23 +1,22 @@
 const Intern = require("../lib/Intern");
 
-test("Can set up school"), () => {
+test("Can set up school", () => {
     const testVal = "Harvard";
    const e = new Intern("Name", 100, "test@test.com", testVal);
    expect(e.school).toBe(testVal);
-};
+});
 
 
+test("getRole() returns \"Intern\"" , () => {
+    const testVal = "Intern";
+   const e = new Intern("Name", 100, "test@test.com", "Harvard" );
+   expect(e.getRole()).toBe(testVal);
+});
 
-// TODO: ______ should return Engineer 
-// const testVal = "engineer"
-// const e = new Engineer("Name", 100, "test@test.com", GithubUsername);
-// expect(e.functionName).toBe(testVal);
 
+test("getSchool() returns school", () => {
+    const testVal = "Harvard";
+   const e = new Intern("Name", 100, "test@test.com", testVal );
+   expect(e.getSchool()).toBe(testVal);
+});
 
-// // TODO: 
-// test("Can get school via _____()") (functionName), () => {
-// const testVal = "Harvard";
-//    const e = new Intern("Name", 100, "test@test.com", testVal);
-// //    expect(e.FunctionName).toBe(testVal);
-
-// }
--- tests/Manager.test.js
+++ tests/Manager.test.js
@@ -2,12 +2,20 @@ const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
 
 test("Can set office number through constructor", () => {
-    const testVal = 50; 
-    const m = new Manager("Name", 70, "test@test.com", testVal);
-    expect(m.OfficeNumber()).toBe(testVal);
+    const testVal = 500; 
+    const e = new Manager("Name", "email@email.com", 70, testVal);
+    expect(e.officeNumber).toBe(testVal);
 });
 
-// TODO: test getRole should return Manager
-// TODO: test can get get office number through getOfficeNumber function
+test("getRole() can return \"Manager\"", () => {
+    const testVal = "Manager";
+    const e = new Manager("Name", "email@email.com", 100, 401);
+    expect(e.getRole()).toBe(testVal);
+});
 
 
+test("Can get office number through getOfficeNumber()", () => {
+    const testVal = 401;
+    const e =  new Manager("Name", "email@email.com", 70, testVal);
+    expect(e.getOfficeNumber()).toBe(testVal);
+});
